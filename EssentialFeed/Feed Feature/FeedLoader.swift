82bce42272c50a3837b39d4da38a7c68d1e837cc
Ario <ariobarxan.dev@gmail.com>
2025-09-06 19:02:27 +1000

    Replace RemoteFeedLoader.Result enum with the predefined LoadFeedResult enum from the Feed Feature module (in preparation to conform to the FeedLoader protocol)

--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -4,11 +4,15 @@
 
 import Foundation
 
-enum LoadFeedResult {
-	case success([FeedItem])
-	case error(Error)
+public enum LoadFeedResult<Error: Swift.Error> {
+    case success([FeedItem])
+    case failure(Error)
 }
 
+extension LoadFeedResult: Equatable where Error: Equatable {}
+
 protocol FeedLoader {
-	func load(completion: @escaping (LoadFeedResult) -> Void)
+    associatedtype Error: Swift.Error
+    
+    func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
 }
