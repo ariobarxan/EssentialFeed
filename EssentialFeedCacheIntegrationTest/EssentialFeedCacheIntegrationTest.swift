6303ebc3f76405e1751427236d2f604531959e25
Ario <ariobarxan.dev@gmail.com>
2025-09-28 16:55:32 +1000

    Add EssentialFeedCacheIntegrationTests target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTest/EssentialFeedCacheIntegrationTest.swift
@@ -0,0 +1,16 @@
+//
+//  EssentialFeedCacheIntegrationTest.swift
+//  EssentialFeedCacheIntegrationTest
+//
+//  Created by Ario Liyan on 27/9/2025.
+//
+
+import Testing
+
+struct EssentialFeedCacheIntegrationTest {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
