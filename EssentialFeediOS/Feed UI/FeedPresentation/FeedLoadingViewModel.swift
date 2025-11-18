d438df7f348a313dca1ad2df7f03205aa2cba907
Ario <ariobarxan.dev@gmail.com>
2025-11-18 12:13:54 +1100

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed UI/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Ario Liyan on 18/11/2025.
+//
+
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed UI/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Ario Liyan on 18/11/2025.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
